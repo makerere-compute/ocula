d5c284bd470ca740b7c7d56d72e36fe936304c14
jqug <jquinn@cit.mak.ac.ug>
2011-10-25 15:23:23 +0300

    better file tracking in training dir

--- src/training/dotraining.sh
+++ src/training/dotraining.sh
@@ -2,8 +2,12 @@
 
 TRAIN_DIR=/home/jq/dev/ocula/data
 
-#opencv_createsamples -info $TRAIN_DIR/info.dat -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40 -num `ls $TRAIN_DIR/train-positive/*.xml | wc -l`
+rm -f $TRAIN_DIR/malariatrain.vec
 
-opencv_traincascade -featureType LBP -data $TRAIN_DIR/cascade -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40
+opencv_createsamples -info $TRAIN_DIR/info.dat -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40 -num `ls $TRAIN_DIR/train-positive/*.xml | wc -l`
 
-#./convert_cascade --size="40x40" $TRAIN_DIR/haarcascade $TRAIN_DIR/haarcascade_plasmodium.xml
+mkdir -p $TRAIN_DIR/cascade
+rm -f $TRAIN_DIR/cascade/stage* $TRAIN_DIR/cascade/params.xml $TRAIN_DIR/cascade/cascade.xml
+
+opencv_traincascade -w 40 -h 40 -featureType LBP -maxFalseAlarmRate 0.5 -maxDepth 5 -data $TRAIN_DIR/cascade -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt
+
