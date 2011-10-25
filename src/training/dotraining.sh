2bc86667ccf9a212d0cdfbd87047d4d05766652d
jqug <jquinn@cit.mak.ac.ug>
2011-10-25 12:40:51 +0300

    training with traincascade implemented

--- src/training/dotraining.sh
+++ src/training/dotraining.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TRAIN_DIR=/home/jq/data/malaria
+TRAIN_DIR=/home/jq/dev/ocula/data
 
-opencv_createsamples -info $TRAIN_DIR/info.dat -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40
+#opencv_createsamples -info $TRAIN_DIR/info.dat -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40 -num `ls $TRAIN_DIR/train-positive/*.xml | wc -l`
 
-opencv_haartraining -data $TRAIN_DIR/haarcascade -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40 -nonsym -mem 512
+opencv_traincascade -featureType LBP -data $TRAIN_DIR/cascade -vec $TRAIN_DIR/malariatrain.vec -bg $TRAIN_DIR/bg.txt -w 40 -h 40
 
 #./convert_cascade --size="40x40" $TRAIN_DIR/haarcascade $TRAIN_DIR/haarcascade_plasmodium.xml
